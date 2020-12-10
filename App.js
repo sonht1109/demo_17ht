b3a3980f3726f315675554b0c7985c347ced117b
sonht <sonht1109@gmail.com>
2020-12-10 17:59:12 +0700

    build frame

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { View } from 'react-native';
+import Signup from './pages/Signup';
 
-const App = ()=>{
-  return(
-    <View></View>
+const App = () => {
+  return (
+    <>
+      <Signup />
+    </>
   )
 }
 
